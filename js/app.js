a611fa693b302ac0b164e44b6cc308077935a63f
Rajesh Kumar <rajesh@thewebplant.in>
2021-07-24 18:54:56 +0530

    a quick and temporary  portfolio

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const name = document.querySelector('.name')
+const email = document.querySelector('.email')
+const msg = document.querySelector('.msg')
+const submitBtn = document.querySelector('.submit')
+
+console.log(name, email, msg, submitBtn)
+
+submitBtn.addEventListener('click', () => {
+
+})
